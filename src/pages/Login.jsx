0375a29b08affc5bebb5b9fad88f3fecfe474484
kangyoungmin <kangym855@gmail.com>
2023-11-29 16:12:31 +0900

    1129_start

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,34 +12,19 @@ import ButtonType from '../components/ButtonType'
 const Login = () => {
   return (
     <>
-     <div className="loginContainer">
-       <div className="logo" style={{backgroundImage:`url(${logo})`}}></div>
-       <h1 className="text-center">MyChat Login</h1>
-       <form className="loginForm">
-       <InputType types="text"
-                  names="useremail"
-                  values=""
-                  placeholders="이메일"
-                  classNames="input"
-       />  
-       <InputType types="password"
-                  names="userpass"
-                  values=""
-                  placeholders="비밀번호"
-                  classNames="input"
-                  functions=""
-       />
-       <ButtonType types="submit"
-                   classNames="button"
-                   functions=""
-                   text="로그인"
-       />          
-       </form>
-       <img src={googleLogin} alt="구글로그인" className="google-login"/>
+      <div className="loginContainer">
+        <div className="logo" style={{ backgroundImage: `url(${logo})` }}></div>
+        <h1 className="text-center">MyChat Login</h1>
+        <form className="loginForm">
+          <InputType types="text" names="useremail" values="" placeholders="이메일" classNames="input" />
+          <InputType types="password" names="userpass" values="" placeholders="비밀번호" classNames="input" functions="" />
+          <ButtonType types="submit" classNames="button" functions="" text="로그인" />
+        </form>
+        <img src={googleLogin} alt="구글로그인" className="google-login" />
       </div>
       <Link to="/join">회원가입</Link>
     </>
-    )
+  )
 }
 
 export default Login
